c41c055b8aa4e76dbbc82d994c43e2d8f9582d30
carlospdt <carlos.pina.torre@gmail.com>
2020-09-29 12:53:15 +0200

    Commit final

--- script2.sh
+++ script2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Script: script1
+# Script: script2
 #Descripción: Muestra mensaje bienvenida al usuario
 #Fecha: 29/09/2020
 #Autor: Carlos Piña
---
+++ script3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Script: script3
+#Descripción: Muestra mensaje 
+#Fecha: 29/09/2020
+#Autor: Carlos Piña
+
+ls
+date
