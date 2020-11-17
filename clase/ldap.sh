24d823c57004d5f90fc9e053e675f38ad307e420
carlospdt <carlos.pina.torre@gmail.com>
2020-11-17 13:47:03 +0100

    17/nov/20

--- clase/ldap.sh
+++ clase/ldap.sh
@@ -9,5 +9,9 @@ do
   echo $TARGET_USER
   USER_ID="$(echo "$TARGET_USER" | cut -d':' -f1)"
   echo USER_ID=$USER_ID
+
+USER_NAME="$(echo "TARGET_USER" | cut -d':' -f5 | cut -d',' f1)"
+ [ ! "USER_NAME" ] && USER_NAME="USER_ID"
+
 done
 )
