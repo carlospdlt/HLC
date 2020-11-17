98f2f518679b6ce12376950c117da9b9640cda50
carlospdt <carlos.pina.torre@gmail.com>
2020-11-17 13:33:42 +0100

    17/nov/20

--- clase/ldap.sh
+++ clase/ldap.sh
@@ -4,8 +4,10 @@
 #Fecha:17/nov/20
 #Autor: Carlos Piña
 
-grep "x:[1-9][0-9][0-9] /etc/passwd | (while read TARGET_USER
+grep "x:[1-9][0-9][0-9]" /etc/passwd | (while read TARGET_USER
 do
-  read $TARGET_USER
+  echo $TARGET_USER
+  USER_ID="$(echo "$TARGET_USER" | cut -d':' -f1)"
+  echo USER_ID=$USER_ID
 done
 )
