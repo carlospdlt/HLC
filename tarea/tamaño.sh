5e91ce4b2b44ee3f8009e496d9848461eaa583d5
carlospdt <carlos.pina.torre@gmail.com>
2020-11-24 12:33:23 +0100

    24/nov/20

--- tarea/tamaño.sh
+++ tarea/tamaño.sh
@@ -7,13 +7,13 @@
 if [ -d $1 ]
 then
    tam=$(du -sm $1 | cut -f 1)
-   echo "El directorio $1 ocupa: $tam MB"
+   echo "El directorio $i ocupa: $tam MB"
 
 elif [ -f $1 ]
 then
    tam=$(du -sm $1 | cut -f 1)
-   echo "El fichero $1 ocupa: $tam MB"
+   echo "El fichero $i ocupa: $tam MB"
 
 else
- echo "$1 No exite o no es un tipo de argumento valido"
+ echo "$i No exite o no es un tipo de argumento valido"
 fi
